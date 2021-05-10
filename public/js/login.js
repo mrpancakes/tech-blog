869a0f561e12a700ce54e526047da9277f9c093b
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-05-10 15:00:32 -0500

    Added login and logout js files, updated login view, tweaked routes. Experiencing issue with users trying to login

---
+++ public/js/login.js
@@ -0,0 +1,58 @@
+// import { json } from "sequelize/types";
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login');
+    const password = document.querySelector('#password-login');
+
+    if (username && password) {
+        const response = await fetch('api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+            console.log('you are logged in');
+        } else {
+            alert('Failed to login. Please try again, or signup.');
+            console.log(response);
+        };
+
+    };
+
+    console.log(username.value);
+    console.log(password.value);
+};
+
+const signUpFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if(email && username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ email, username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if(response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up');
+        }
+    }
+}
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+  document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signUpFormHandler);
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
-const routes = require('./controllers/home-routes');
+// const routes = require('./controllers/home-routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const helpers = require('./utils/helpers');
